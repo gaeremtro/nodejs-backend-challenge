84af832d92ea38bf6c2d0cbee9fff2506bbe4ebb
gaeremtro <gabripenalva42@gmail.com>
2021-12-22 13:23:34 +0100

    delete conexión.ts

--- connexion.ts
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017').
-catch ((error:any):void => {
-    console.log('some think', error);
-})
-
-export default mongoose;
